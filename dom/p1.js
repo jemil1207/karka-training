0b696c4fc5e3b34846b2211aed9ccdc48748babf
jemil1207 <jemil12072001@gmail.com>
2024-02-12 16:54:15 +0530

    dom

---
+++ dom/p1.js
@@ -0,0 +1,8 @@
+function clk(){
+   let color=document.getElementById("box")
+    color.style.backgroundColor="yellow"
+}
+function out(){
+    let color=document.getElementById("box")
+     color.style.backgroundColor="red"
+ }
